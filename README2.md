ad9fe159e51f26d231cf83ec67f732a923c6eb4d
Gabriel Rivas <55334104+Gabriel-R285@users.noreply.github.com>
2025-06-11 21:41:37 +0000

    second commit

---
+++ README2.md
@@ -0,0 +1,5 @@
+1. le das cd /project
+2. Luego en la terminal colocar el comando "python manage.py runserver"
+3. Cuando la sesion se incie, va a aparecer un mensaje que dice inciar aplicacion en el puerto 8000. Dsrle click.
+4. Luego colocar la ruta del login: /login/
+5. Inicir sesion con usuario y contrasena. 
--- project/project/urls.py
+++ project/project/urls.py
@@ -20,7 +20,7 @@ from django.contrib.auth.views import LoginView, LogoutView
 
 
 urlpatterns = [
-    path('', views.home, name='home'),    
+    path('', views.home, name=''),    
     path('login/', LoginView.as_view(template_name='login.html'), name='login'),
     path('profile/', views.myProfile, name="myProfile"),
     path('error/', views.error, name="error"),
